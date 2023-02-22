14461efe99d6cbc5e0773bb20f9cea4642905eec
mirandaweston <mweston105@yahoo.co.uk>
2023-02-22 11:55:25 +0000

    Updating searchCandies test suite: second test passed

--- searchCandies/__tests__/searchCandies.test.js
+++ searchCandies/__tests__/searchCandies.test.js
@@ -4,4 +4,8 @@ describe('searchCandies', () => {
   it('returns "Mars" and "Maltesers" when search string is "Ma" and max price is 10', () => {
     expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers']);
   });
+
+  it('returns "Mars" when search string is "Ma" and max price is 2', () => {
+    expect(searchCandies('Ma', 2)).toEqual(['Mars']);
+  });
 });
