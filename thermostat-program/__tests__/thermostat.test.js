8350a7f1c43063180a71a0ea89af1d80c838e0d3
mirandaweston <mweston105@yahoo.co.uk>
2023-02-22 22:44:15 +0000

    Updating Thermostat class and test suite: passes 'up' function

--- thermostat-program/__tests__/thermostat.test.js
+++ thermostat-program/__tests__/thermostat.test.js
@@ -14,4 +14,34 @@ describe('Thermostat', () => {
     expect(thermostat.getTemperature()).toBe(10);
   });
 });
+
+describe('up', () => {
+  it('increases the temperature by 1 degree when powermode is on and temperature is lower than or equal to 24', () => {
+    const thermostat = new Thermostat();
+    thermostat.temperature = 23;
+    thermostat.powermode = true;
+    expect(thermostat.up()).toBe(24);
+  });
+  
+  it('increases the temperature by 1 degree when powermode is off and temperature is lower than or equal to 31', () => {
+    const thermostat = new Thermostat();
+    thermostat.temperature = 30;
+    thermostat.powermode = false;
+    expect(thermostat.up()).toBe(31);
+  });
+  
+  it('returns the temperature when powermode is on and temperature is higher than 24', () => {
+    const thermostat = new Thermostat();
+    thermostat.temperature = 25;
+    thermostat.powermode = true;
+    expect(thermostat.up()).toBe(25);
+  });
+  
+  it('returns the temperature when powermode is off and temperature is higher than 31', () => {
+    const thermostat = new Thermostat();
+    thermostat.temperature = 23;
+    thermostat.powermode = true;
+    expect(thermostat.up()).toBe(24);
+  });
+  });
 });
--- thermostat-program/src/thermostat.js
+++ thermostat-program/src/thermostat.js
@@ -7,6 +7,19 @@ class Thermostat {
   getTemperature() {
     return this.temperature
   }
+
+  up() {
+    if (this.powermode === true) {
+      if (this.temperature <= 24) {
+        this.temperature += 1;
+      }
+    } else {
+      if (this.temperature <= 31) {
+        this.temperature += 1;
+      }
+    }
+    return this.temperature;
+  }
 }
 
 module.exports = Thermostat;
